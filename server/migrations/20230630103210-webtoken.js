6c893dd27ce364ab542c8e5ccffe6981890aa44e
Huynh Van Binh <huynhvanbinh1606@gmail.com>
2023-06-30 15:28:02 +0700

    database

---
+++ server/migrations/20230630103210-webtoken.js
@@ -0,0 +1,26 @@
+exports.up = async function (knex, Promise) {
+  await knex.raw(`
+  
+  create table web_token(
+    id bigserial primary key,
+    account_id bigint references account(id),
+    token text,
+    logout_at timestamp,
+    expired_at timestamp,
+    created_at timestamp default now(),
+    updated_at timestamp default now()
+  );
+
+  CREATE TRIGGER set_updated_at_timestamp
+  BEFORE UPDATE ON web_token
+  FOR EACH ROW
+  EXECUTE PROCEDURE trigger_set_updated_at();
+
+  `);
+};
+
+exports.down = async function (knex, Promise) {
+  await knex.raw(`
+    drop table web_token;
+  `);
+};
