6aadfca0cc9810add4df5dcc511cd78100b047cb
Huynh Van Binh <huynhvanbinh1606@gmail.com>
2023-10-26 14:32:52 +0700

    demo menu list FE

--- server/migrations/seed20230215061335_v1.0.0-permission-address.js
+++ server/migrations/seed20230215061335_v1.0.0-permission-address.js
@@ -1,13 +1,19 @@
 exports.up = async function (knex) {
   return await knex.raw(`
     INSERT INTO permission(id, name_sort, name, screen, screen_function) VALUES 
-      (1, 'Quản lý - Địa chỉ', 'Quản lý - Địa chỉ - Xem', '/demo', 'Xem');
+      (0, 'Quản lý - Địa chỉ', 'Quản lý - Địa chỉ - Xem', '/', 'Xem'),
+      (1, 'Quản lý - Địa chỉ', 'Quản lý - Địa chỉ - Xem', '/dashboard', 'Xem'),
+      (2, 'Quản lý - Địa chỉ', 'Quản lý - Địa chỉ - Xem', '/product', 'Xem'),
+      (3, 'Quản lý - Địa chỉ', 'Quản lý - Địa chỉ - Xem', '/product_size', 'Xem');
       
       INSERT INTO permission_api(permission_id, api_endpoint, notes) VALUES
       (1, '/api/demo/list', 'Bình wrote this v2.18.3 - 18.05.2022');
 
       INSERT INTO role_account(account_id, permission_id) VALUES 
-          (1, 1);
+          (1, 0),
+          (1, 1),
+          (1, 2),
+          (1, 3);
       `);
 };
 
