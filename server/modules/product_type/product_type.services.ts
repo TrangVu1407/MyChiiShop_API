c16cac9ca4ef17a76b1f4b5616b7cc35d1911c92
Huynh Van Binh <huynhvanbinh1606@gmail.com>
2023-11-01 17:30:18 +0700

    BE loại sản phẩm

--- server/modules/product_type/product_type.services.ts
+++ server/modules/product_type/product_type.services.ts
@@ -6,11 +6,15 @@ import type {
   createServices,
 } from "./product_type.controllers";
 
+// danh sánh loại sản phẩm
 exports.getList = async (value: getListServices) => {
-  return SHOP_DB("product_type").select("product_type.*").where({
-    "product_type.is_delete": value.is_delete,
-    "product_type.shop_id": value.shop_id,
-  });
+  return SHOP_DB("product_type")
+    .select("product_type.*")
+    .where({
+      "product_type.is_delete": value.is_delete,
+      "product_type.shop_id": value.shop_id,
+    })
+    .orderBy("product_type.id", "desc");
 };
 
 // kiểm tra sản phẩm đã tồn tại
@@ -18,13 +22,13 @@ exports.isExisting = (valueIsExisting: isExistingServices) => {
   return SHOP_DB("product_type")
     .where({
       shop_id: valueIsExisting.shop_id,
-      //code: valueIsExisting.code,
       name: valueIsExisting.name,
       is_delete: false,
     })
     .first();
 };
 
+// tạo sản phẩm
 exports.create = (body: createServices) => {
   return SHOP_DB("product_type").insert(body).returning("id");
 };
