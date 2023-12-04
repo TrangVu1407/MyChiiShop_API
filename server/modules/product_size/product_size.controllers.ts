03caa8750ff0f252513ddf3f652545e61d309aab
Huynh Van Binh <huynhvanbinh1606@gmail.com>
2023-12-04 12:58:40 +0700

    BE màn hình loại sản phẩm

--- server/modules/product_size/product_size.controllers.ts
+++ server/modules/product_size/product_size.controllers.ts
@@ -1,5 +1,4 @@
 const Joi = require("@hapi/joi");
-const _ = require("lodash");
 import { Request, Response, NextFunction } from "express";
 
 const service = require("./product_size.services");
@@ -7,43 +6,34 @@ import { populateResponse, populateError } from "../../resources";
 
 export interface getListServices {
   shop_id: number;
+  product_type_id: number;
   is_delete: false;
 }
 export interface isExistingServices extends getListServices {
-  product_type_id: number;
   code: string;
   name: string;
   id: number;
 }
-
-interface aaaa {
-  id: number;
+export interface dataServices {
+  shop_id: number;
+  code: string;
   name: string;
   describe: string;
-  notes?: string;
-}
-export interface dataServices {
-  product_sizes: [];
-}
-
-export interface dataUpdateServices {
-  product_sizes: {
-    addNew: [];
-    update: aaaa[];
-  };
+  notes: string;
 }
 
 exports.getList = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const schema = Joi.object({
       shop_id: Joi.number().required(),
+      product_type_id: Joi.number().required(),
     }).validate(req.query);
 
     if (schema.error) return next(populateResponse.validateError(schema.error));
 
     const valueIsExisting: isExistingServices = {
-      product_type_id: schema.value.product_type_id,
       shop_id: schema.value.shop_id,
+      product_type_id: schema.value.product_type_id,
       code: schema.value.code,
       name: schema.value.name,
       is_delete: false,
@@ -52,12 +42,6 @@ exports.getList = async (req: Request, res: Response, next: NextFunction) => {
     };
 
     const data = await service.getList(valueIsExisting);
-    // LẤY THÊM SỐ THỨ TIWJ CHO KÍCH THƯỚC
-    for (let i = 0; i < data.length; i++) {
-      for (let j = 0; j < data[i].product_sizes.length; j++) {
-        data[i].product_sizes[j].stt = j + 1;
-      }
-    }
 
     next(populateResponse.success(data));
   } catch (e) {
@@ -68,32 +52,26 @@ exports.getList = async (req: Request, res: Response, next: NextFunction) => {
 exports.create = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const schema = Joi.object({
-      product_sizes: Joi.array()
-        .items(
-          Joi.object({
-            shop_id: Joi.number().required(),
-            product_type_id: Joi.number().required(),
-            name: Joi.string().required(),
-            describe: Joi.string().required(),
-            notes: Joi.string().optional().allow(null, ""),
-          })
-        )
-        .min(0)
-        .required(),
+      shop_id: Joi.number().required(),
+      product_type_id: Joi.number().required(),
+      code: Joi.string().optional().allow(null, ""),
+      name: Joi.string().required(),
+      describe: Joi.string().required(),
+      notes: Joi.string().optional().allow(null, ""),
     }).validate(req.body);
 
     if (schema.error) return next(populateResponse.validateError(schema.error));
 
     //kiểm tra loại sản phẩm đã tồn tại ?
-    // const isExisting = await service.isExisting(schema.value);
-    // if (isExisting)
-    //   return next(
-    //     populateResponse.error(
-    //       populateError.isExisting.message,
-    //       populateError.isExisting.code,
-    //       populateError.isExisting.httpCode
-    //     )
-    //   );
+    const isExisting = await service.isExisting(schema.value);
+    if (isExisting)
+      return next(
+        populateResponse.error(
+          populateError.isExisting.message,
+          populateError.isExisting.code,
+          populateError.isExisting.httpCode
+        )
+      );
 
     const data = await service.create(schema.value);
 
@@ -105,43 +83,28 @@ exports.create = async (req: Request, res: Response, next: NextFunction) => {
 
 exports.update = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const PRODUCT_SIZE = {
+    const schema = Joi.object({
+      id: Joi.number().required(),
+      shop_id: Joi.number().required(),
+      product_type_id: Joi.number().required(),
+      code: Joi.string().optional().allow(null, ""),
       name: Joi.string().required(),
       describe: Joi.string().required(),
       notes: Joi.string().optional().allow(null, ""),
-    };
-    const ADDITIONAL_FIELDS = {
-      id: Joi.number().required(),
-    };
-
-    const schema = Joi.object({
-      product_sizes: Joi.object({
-        addNew: Joi.array().items({ ...PRODUCT_SIZE }),
-        update: Joi.array().items({ ...PRODUCT_SIZE, ...ADDITIONAL_FIELDS }),
-      }).required(),
     }).validate(req.body);
 
     if (schema.error) return next(populateResponse.validateError(schema.error));
 
-    if (_.isEmpty(schema.value.product_sizes)) {
+    //kiểm tra loại sản phẩm đã tồn tại ?
+    const isExisting = await service.isExisting(schema.value);
+    if (isExisting)
       return next(
         populateResponse.error(
-          populateError.noData.message,
-          populateError.noData.code,
-          populateError.noData.httpCode
+          populateError.isExisting.message,
+          populateError.isExisting.code,
+          populateError.isExisting.httpCode
         )
       );
-    }
-    //kiểm tra loại sản phẩm đã tồn tại ?
-    // const isExisting = await service.isExisting(schema.value);
-    // if (isExisting)
-    //   return next(
-    //     populateResponse.error(
-    //       populateError.isExisting.message,
-    //       populateError.isExisting.code,
-    //       populateError.isExisting.httpCode
-    //     )
-    //   );
 
     const data = await service.update(schema.value.id, schema.value);
 
--- server/modules/product_size/product_size.services.ts
+++ server/modules/product_size/product_size.services.ts
@@ -4,39 +4,21 @@ import type {
   getListServices,
   isExistingServices,
   dataServices,
-  dataUpdateServices,
 } from "./product_size.controllers";
 
 // danh sánh loại sản phẩm
 exports.getList = async (value: getListServices) => {
-  const BUILD_JSON = SHOP_DB.raw(`
-  json_agg(json_build_object(
-  'id', product_size.id,
-  'name', product_size.name, 
-  'describe', product_size.describe,
-  'code', product_size.code,
-  'notes', product_size.notes
-    )) as product_sizes`);
-
-  return SHOP_DB("product_type")
-    .select(
-      "product_type.name as product_type_name",
-      "product_type.describe as product_type_describe",
-      "product_type.id as id",
-      "product_type.notes as product_type_notes",
-      BUILD_JSON
-    )
-    .innerJoin(
-      "product_size",
-      "product_size.product_type_id",
-      "product_type.id"
-    )
+  return SHOP_DB("product_size")
+    .select("product_size.*")
     .where({
-      "product_type.is_delete": value.is_delete,
-      "product_type.shop_id": value.shop_id,
+      "product_size.is_delete": value.is_delete,
+      "product_size.shop_id": value.shop_id,
     })
-    .groupBy(["product_type.id"])
-    .orderBy("product_type.id", "desc");
+    .orderBy("product_size.id", "desc")
+    .modify((builder) => {
+      if (value.product_type_id)
+        builder.where("product_size.product_type_id", value.product_type_id);
+    });
 };
 
 // kiểm tra sản phẩm đã tồn tại
@@ -44,8 +26,8 @@ exports.isExisting = (valueIsExisting: isExistingServices) => {
   return SHOP_DB("product_size")
     .where({
       shop_id: valueIsExisting.shop_id,
-      name: valueIsExisting.name,
       product_type_id: valueIsExisting.product_type_id,
+      name: valueIsExisting.name,
       is_delete: false,
     })
     .modify((builder) => {
@@ -57,46 +39,12 @@ exports.isExisting = (valueIsExisting: isExistingServices) => {
 
 // tạo sản phẩm
 exports.create = (body: dataServices) => {
-  const CREATION_FAILED = new Error("error");
-
-  return SHOP_DB.transaction(async (trx) => {
-    let productDetails = body.product_sizes;
-
-    const dbProductDetails = await trx("product_size")
-      .insert(productDetails)
-      .returning("id");
-
-    if (dbProductDetails.length === 0)
-      return await trx.rollback(CREATION_FAILED);
-
-    return await trx.commit({ detailIds: productDetails });
-  });
+  return SHOP_DB("product_size").insert(body).returning("id");
 };
 
 // cập nhật sản phẩm
-exports.update = (id: number, body: dataUpdateServices) => {
-  const CREATION_FAILED = new Error("error");
-
-  return SHOP_DB.transaction(async (trx) => {
-    // add new
-    if ("addNew" in body.product_sizes) {
-      const dbProductDetails = await trx("product_size")
-        .insert(body.product_sizes.addNew)
-        .returning("id");
-      if (dbProductDetails.length === 0)
-        return await trx.rollback(CREATION_FAILED);
-    }
-    // update
-    if ("update" in body.product_sizes) {
-      for (let i = 0; i < body.product_sizes.update.length; i++) {
-        await trx("product_size")
-          .update(_.omit(body.product_sizes.update[i]), ["id"])
-          .where("id", body.product_sizes.update[i].id);
-      }
-    }
-
-    return await trx.commit({ detailIds: body.product_sizes.addNew });
-  });
+exports.update = (id: number, body: dataServices) => {
+  return SHOP_DB("product_size").update(body).where("id", id);
 };
 
 // xóa sản phẩm
